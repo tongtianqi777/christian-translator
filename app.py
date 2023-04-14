e9be6f335e51cd5007b0e0427e90fb0de274b7d5
Tianqi Tong <tianqi_tong@apple.com>
2023-04-13 23:36:17 -0700

    use waitress as server

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def split_multilingual_text(text):
 
 # run the app.
 if __name__ == "__main__":
-    app.debug = False
-    app.run(port=8000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8000)
