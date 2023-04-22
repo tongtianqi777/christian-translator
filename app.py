923833658a82872c15ecf681e79898418e80f19d
tongtianqi777 <tianqitong@outlook.com>
2023-04-22 16:40:22 -0700

    switch to gunicorn

--- app.py
+++ app.py
@@ -142,5 +142,5 @@ if __name__ == "__main__":
         app.debug = True
         app.run(port=8000)
     else:
-        from waitress import serve
-        serve(app, host="0.0.0.0", port=8000)
+        app.debug = False
+        app.run(host='0.0.0.0', port=8000)
