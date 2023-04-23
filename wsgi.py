aebed60ed97fb6f89ac06ef4e0641815d5df52d7
tongtianqi777 <tianqitong@outlook.com>
2023-04-22 17:42:37 -0700

    update template

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
+import platform
+
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    if platform.system() == "Darwin":
+        # enable debug mode if it's mac
+        app.debug = True
+        app.run(port=8000)
+    else:
+        app.debug = False
+        app.run(host='0.0.0.0', port=8000)
