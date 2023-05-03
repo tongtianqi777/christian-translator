95f534b8cf9c48529b38ee1ca32e31857af12a54
tongtianqi777 <tianqitong@outlook.com>
2023-05-03 15:09:37 -0700

    update app

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,12 @@
 import platform
 
-from app import app
+from application import app
 
 if __name__ == "__main__":
     if platform.system() == "Darwin":
         # enable debug mode if it's mac
         app.debug = True
-        app.run(port=8000)
+        app.run()
     else:
         app.debug = False
-        app.run(host='0.0.0.0', port=8000)
+        app.run()
